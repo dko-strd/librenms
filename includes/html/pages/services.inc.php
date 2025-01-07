7b702587d79e22b6aaa8946d87b5b8fc69fe03f9
Daniel Koehler <daniel.koehler@rendsburg.de>
2025-01-07 10:38:05 +0100

    fixes small display issue in services overview
    suppress output of column "Last Changed" in services overview, when the service has not been changed since added to a host.

--- includes/html/pages/services.inc.php
+++ includes/html/pages/services.inc.php
@@ -192,7 +192,7 @@ require_once 'includes/html/modal/delete_service.inc.php';
                         echo '<td>' . nl2br(\LibreNMS\Util\Clean::html($service['service_ip'], [])) . '</td>';
                         echo '<td>' . nl2br(\LibreNMS\Util\Clean::html($service['service_message'], [])) . '</td>';
                         echo '<td>' . nl2br(\LibreNMS\Util\Clean::html($service['service_desc'], [])) . '</td>';
-                        echo '<td>' . \LibreNMS\Util\Time::formatInterval(time() - $service['service_changed']) . '</td>';
+                        echo '<td>' . ($service['service_changed'] == 0 ? '' : \LibreNMS\Util\Time::formatInterval(time() - $service['service_changed'])) . '</td>';
 
                         $service_checked = '';
                         $ico = 'pause';
